dad26c4085999f00bb8e7454bcbed6a172f80a11
Omar Abdullah <omar.abdallah5209@gmail.com>
2025-09-20 23:26:45 +0300

    deploy

--- tajer-platform-main/app/layout.tsx
+++ tajer-platform-main/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
-import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Tajer Platform',
